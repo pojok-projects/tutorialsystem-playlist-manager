9989c5a91063fd065f9b8c4d4502d398b5ad3215
Juni Yadi <juniyadi94@gmail.com>
2019-09-19 16:30:27 +0700

    CM-21 fix(pm) - Fixed Route Playlists

--- routes/pm/v1/playlists.js
+++ routes/pm/v1/playlists.js
@@ -4,5 +4,7 @@ const { PlaylistsController } = require('../../../controller')
 
 playlists.get('/:userid', PlaylistsController.index)
 playlists.post('/:userid/create', PlaylistsController.create)
+playlists.post('/:userid/update/:playlistsid', PlaylistsController.update)
+playlists.post('/:userid/delete/:playlistsid', PlaylistsController.delete)
 
 module.exports = playlists
