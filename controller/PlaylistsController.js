2a1f8c2dc0d65b91a1b2943021bc4395fe620091
Juni Yadi <juniyadi94@gmail.com>
2019-10-14 08:37:24 +0700

    CM-21 fix(pm): Update Playlists Controller

--- controller/PlaylistsController.js
+++ controller/PlaylistsController.js
@@ -1,28 +1,35 @@
 const axios = require('axios')
 const uuid = require('uuid/v4')
+const _ = require('underscore')
 const dotenv = require('dotenv')
 dotenv.config()
 
 const apidbil = process.env.ENDPOINTAPI_DBIL
-const { groupData } = require('./HelpersController')
 
 module.exports = {
     index: async(req, res, next) => {
         try {
-            const axiosReq = await axios.get(apidbil + '/user/' + req.params.userid)
+            const axiosReq = await axios.get(apidbil + 'user/' + req.params.userid)
 
-            if(axiosReq.status === 200) {
-                const groupByPCID = groupData('playlistcategory_id')
-                res.send({
-                    status : {
-                        code: 200,
-                        message: 'success'
-                    },
-                    data: groupByPCID(axiosReq.data.playlists)
-                })
-            } else {
-                throw new Error(axiosReq)
-            }
+                if(axiosReq.status === 200) {
+                    const allplaylist = axiosReq.data.playlists
+
+                    if(allplaylist !== null) {
+                        const grsoData =  _.groupBy(_.sortBy(axiosReq.data.playlists, 'order_list'), 'playlistcategory_id');
+
+                        res.send({
+                            status : {
+                                code: 200,
+                                message: 'success'
+                            },
+                            data: grsoData
+                        })
+                    } else {
+                        throw new Error('Playlists Not Found..!!')
+                    }
+                } else {
+                    throw new Error(axiosReq)
+                }
         } catch (err) {
             next(err)
         }
@@ -30,7 +37,7 @@ module.exports = {
     create: async(req, res, next) => {
         try {
             // get all playlists data
-            const axiosReq = await axios.get(apidbil + '/user/' + req.params.userid)
+            const axiosReq = await axios.get(apidbil + 'user/' + req.params.userid)
             const allplaylist = axiosReq.data.playlists
 
             // get form input
@@ -50,28 +57,29 @@ module.exports = {
                 updated_at: now
             }
 
-            // check if duplicate data in playlists
-            // find metadata_id and playlistcategory_id
-            const findplaylists = allplaylist.filter((items) => {
-                return (items.metadata_id === metadataid && items.playlistcategory_id === categoryid)
-            })
-
-            if(findplaylists.length > 0) {
-                throw new Error('Skip, Duplicated Data')
-            }
-
-            // check if allplaylists is null or not
-            // if not null, then merged array allplaylists
-            // with object from newplaylists
-            // then in data post, check again for post first time or not
             if(allplaylist !== null) {
+
+                // check if duplicate data in playlists
+                // find metadata_id and playlistcategory_id
+                const findplaylists = _.filter(allplaylist, (items) => {
+                    return (items.metadata_id === metadataid && items.playlistcategory_id === categoryid)
+                })
+
+                if(findplaylists.length > 0) {
+                    throw new Error('Skip, Duplicated Data')
+                }
+
+                // check if allplaylists is null or not
+                // if not null, then merged array allplaylists
+                // with object from newplaylists
+                // then in data post, check again for post first time or not
                 allplaylist.push(newplaylist)
             }
 
             // update playlists
             const PostData = await axios({
                 method: 'POST',
-                url: apidbil + '/user/update/' + req.params.userid,
+                url: apidbil + 'user/update/' + req.params.userid,
                 headers: {
                     accept: "application/json"
                 },
@@ -88,8 +96,12 @@ module.exports = {
     update: async(req, res,next) => {
         try {
             // get all playlists data
-            const axiosReq = await axios.get(apidbil + '/user/' + req.params.userid)
+            const axiosReq = await axios.get(apidbil + 'user/' + req.params.userid)
             const allplaylist = axiosReq.data.playlists
+
+            if(allplaylist == null) {
+                throw new Error('Unable update data, data not found..!!')
+            }
 
             // Get id
             const playlistsid = req.params.playlistsid
@@ -101,28 +113,20 @@ module.exports = {
             const now = new Date().toISOString()
 
             // find playlists index key
-            let findplaylists = allplaylist.findIndex(items => items.id === playlistsid)
+            let findplaylists = allplaylist.findIndex(items => items.id == playlistsid)
 
             // update based key
             // update playlistcategory_id if not null
-            if(categoryid !== null) {
-                allplaylist[findplaylists].playlistcategory_id = categoryid
-            }
+            allplaylist[findplaylists].playlistcategory_id = categoryid ? categoryid : allplaylist[findplaylists].playlistcategory_id
 
             // update metadata_id if not null
-            if(metadataid !== null) {
-                allplaylist[findplaylists].metadata_id = metadataid
-            }
+            allplaylist[findplaylists].metadata_id = metadataid ? metadataid : allplaylist[findplaylists].metadata_id
 
             // update order_list if not null
-            if(orderlist !== null) {
-                allplaylist[findplaylists].order_list = orderlist
-            }
+            allplaylist[findplaylists].order_list = orderlist ? orderlist : allplaylist[findplaylists].order_list
 
             // update last_watch if not null
-            if(lastwatch !== null) {
-                allplaylist[findplaylists].last_watch = lastwatch
-            }
+            allplaylist[findplaylists].last_watch = lastwatch ? lastwatch : allplaylist[findplaylists].last_watch
 
             // update updated_at time now
             allplaylist[findplaylists].updated_at = now
@@ -130,7 +134,7 @@ module.exports = {
             // update playlists
             const PostData = await axios({
                 method: 'POST',
-                url: apidbil + '/user/update/' + req.params.userid,
+                url: apidbil + 'user/update/' + req.params.userid,
                 headers: {
                     accept: "application/json"
                 },
@@ -147,12 +151,16 @@ module.exports = {
     delete: async(req, res, next) => {
         try {
             // get all playlists data
-            const axiosReq = await axios.get(apidbil + '/user/' + req.params.userid)
+            const axiosReq = await axios.get(apidbil + 'user/' + req.params.userid)
             const allplaylist = axiosReq.data.playlists
 
             // Get id
             const playlistsid = req.params.playlistsid
             
+            if(allplaylist == null) {
+                throw new Error('Unable delete data, data not found..!!')
+            }
+
             // find playlists id
             const findplaylists = allplaylist.filter((items) => {
                 return items.id !== playlistsid
@@ -161,7 +169,7 @@ module.exports = {
             // update playlists
             const PostData = await axios({
                 method: 'POST',
-                url: apidbil + '/user/update/' + req.params.userid,
+                url: apidbil + 'user/update/' + req.params.userid,
                 headers: {
                     accept: "application/json"
                 },
